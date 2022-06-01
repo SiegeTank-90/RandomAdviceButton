e4d8d86fafff57d355434985a2626b1f2ce7b703
SiegeTank-90 <ccline140@yahoo.com>
2022-06-01 10:03:53 -0500

    Adding A Button Delay, Because of the Caching change limits to 2 a change every 2 seconds.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import dice from "./images/icon-dice.svg";
 import desktopDivider from "./images/pattern-divider-desktop.svg";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
 
   const [adviceMessage, setAdviceMessage] = useState("");
   const [adviceID, setAdviceID] = useState("");
+  const [activeState, setActiveState] = useState("Randomizer")
 
   async function GetAdvice() {
     let response = await axios.get("https://api.adviceslip.com/advice");
@@ -19,25 +20,25 @@ function App() {
     } catch (error) {
       console.log(error);
     }
+    setActiveState("Randomizer-Deactive")
     setAdviceMessage(response.data.slip.advice);
     setAdviceID(response.data.slip.id);
+    setTimeout( () => {setActiveState("Randomizer")}, 2000)
   }
 
   return (
-  
-      <div className="Container">
-        <div className="MessageContainer">
-          <h3 className="AdviceNum">advice #{adviceID}</h3>
-          <h1 className="AdviceMessage">{adviceMessage}</h1>
-          <div className="Divider">
-            <img src={desktopDivider} alt="divider" />"
-          </div>
+    <div className="Container">
+      <div className="MessageContainer">
+        <h3 className="AdviceNum">advice #{adviceID}</h3>
+        <h1 className="AdviceMessage">{adviceMessage}</h1>
+        <div className="Divider">
+          <img src={desktopDivider} alt="divider" />"
         </div>
-        <button className="Randomizer">
-          <img class src={dice} alt="dice" onClick={() => GetAdvice()} />
-        </button>
       </div>
-   
+      <button className={activeState} onClick={GetAdvice}>
+        <img src={dice} alt="dice"  />
+      </button>
+    </div>
   );
 }
 
